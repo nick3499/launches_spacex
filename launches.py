d8b87c3e5bde0c7c4661a82c20b0053f87dfb6b7
nick3499 <ntrinen@yahoo.com>
2019-12-16 20:13:00 -0600

    revised code
    - revised imports to specify methods
    - added `app.config.from_envvar('LAUNCHES_DEV_ENV')`

--- launches.py
+++ launches.py
@@ -1,13 +1,14 @@
-import json
-import requests
+from json import loads
+from requests import get
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_envvar('LAUNCHES_DEV_ENV')
 
 @app.route('/')
 def index():
-    res = requests.get("https://api.spacexdata.com/v3/launches")
-    data = json.loads(res.text)
+    res = get("https://api.spacexdata.com/v3/launches")
+    data = loads(res.text)
     return render_template("launches.html", data=data)
 
 if __name__ == '__main__':
