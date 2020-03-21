1dcff1244976b0781ff958bb164eed14c22b2e27
nick3499 <ntrinen@yahoo.com>
2020-03-20 22:21:19 -0500

    Revised code and documentation
    - basic script remains unchanged.
    - revised script to pass `pylint` and `flake8` tests.
    - added documentation.
    - added shebang line.

--- launches.py
+++ launches.py
@@ -1,15 +1,23 @@
+#! /bin/python3
+'''`launches` module gets rocket launch data from SpaceX API.'''
 from json import loads
 from requests import get
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
-app.config.from_envvar('LAUNCHES_DEV_ENV')
 
-@app.route('/')
+APP = Flask(__name__)  # create Flask instance
+APP.config.from_envvar('LAUNCHES_DEV_ENV')  # set environmental variable
+
+
+@APP.route('/')  # route decorator
 def index():
-    res = get("https://api.spacexdata.com/v3/launches")
-    data = loads(res.text)
-    return render_template("launches.html", data=data)
+    '''`index()` method gets rocket launch data from the SpaceX API. \
+And renders template for browser display.'''
+    res = get("https://api.spacexdata.com/v3/launches")  # get data from API
+    data = loads(res.text)  # load data
+    return render_template("launches.html", data=data)  # render template
+
 
 if __name__ == '__main__':
-    app.run()
+    APP.run()  # if standalone
